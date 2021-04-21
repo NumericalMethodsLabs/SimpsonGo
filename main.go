4553cddc1580979d5cc9c7f41e7d846d2a4e1b86
Дима <dimadetected@yandex.ru>
2021-04-21 14:15:16 +0300

    init

--- main.go
+++ main.go
@@ -1,54 +1,53 @@
 package main
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
+
+func f(x float64) float64 {
+	return x * x * x
+}
 
 func main() {
 	a := 0.0
-	b := 3.1415926535
-	n := 8
-	e := 0.0001
+	b := 1.0
+	n := 2
+	eps := 10e-5
 
-	h := (b - a) / (2 * float64(n))
-	x := make([]float64, 0)
-	f := make([]float64, 0)
-	for i := 0; i <= 2*n; i++ {
-		x = append(x, a+float64(i)*h)
-		f = append(f, math.Sin(a+float64(i)*h))
-	}
+	h := (b - a) / float64(n)
+	s0, s1, s2 := f(a)+f(b), 0.0, 0.0
 
-	s1 := 0.0
-	for i := 1; i <= 2*n-1; i++ {
-		if i%2 == 1 {
-			s1 = s1 + 4*f[i]
-		}
-	}
-	s2 := 0.0
-	for i := 1; i <= 2*n-1; i++ {
-		if i%2 == 0 {
-			s2 = s2 + 2*f[i]
-		}
+	for i := 1; i <= n/2; i++ {
+		s1 += f(a + (2*float64(i)-1)*h)
 	}
 
-	int1 := (h / 3) * (f[0] + s1 + s2 + f[2*n])
-	n = n * 2
-	h = (b - a) / (2 * float64(n))
-	for i := 0; i < 2*n; i++ {
-		x[i] = a + float64(i)*h
-		f[i] = 1 / (x[i] + 2)
+	for i := 1; i < n/2; i++ {
+		s2 += f(a + 2*float64(i)*h)
 	}
-	s1 = 0.0
-	for i := 1; i <= 2*n-1; i++ {
-		if i%2 == 1 {
-			s1 = s1 + 4*f[i]
-		}
-	}
-	s2 = 0.0
-	for i := 1; i <= 2*n-1; i++ {
-		if i%2 == 0 {
-			s2 = s2 + 2*f[i]
-		}
-	}
-	int2 := (h / 3) * (f[0] + s1 + s2 + f[2*n])
-	pogr := math.Abs(int1 - int2)
 
+	var i1, i2 float64 = h / 3 * (s0 + 4*s1 + 2*s2), h / 3 * (s0 + 4*s1 + 2*s2)
+
+	i1 = i2
+	n *= 2
+	h = (b - a) / float64(n)
+	s2 = s1 + s2
+	s1 = 0
+	for i := 1; i <= n/2; i++ {
+		s1 += f(a + (2*float64(i)-1)*h)
+	}
+	i2 = h / 3 * (s0 + 4*s1 + 2*s2)
+	for math.Abs(i2-i1) > 15.0/16.0*eps {
+		i1 = i2
+		n *= 2
+		h = (b - a) / float64(n)
+		s2 = s1 + s2
+		s1 = 0
+		for i := 1; i <= n/2; i++ {
+			s1 += f(a + (2*float64(i)-1)*h)
+		}
+		i2 = h / 3 * (s0 + 4*s1 + 2*s2)
+	}
+	fmt.Println("Количество:", n)
+	fmt.Println("I:", i2)
 }
